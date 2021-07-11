948bb85d63f54c92bc1ea41ec7063ff60deb76bc
Kosowski <lipekosowski12@gmail.com>
2021-07-10 23:57:02 -0300

    add static request

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-import axios from "axios";
+var axios = require('axios')
+
+axios.get('https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=11007')
+.then(function(response){
+    var data = response.data;
+
+    console.log(data);
+})
+.catch(function(err){
+    console.error(err);
+})
